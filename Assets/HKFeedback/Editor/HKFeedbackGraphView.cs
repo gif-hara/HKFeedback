c9aec4e891bb265ae23aea0aba6c103f3552417b
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-03-19 23:12:10 +0900

    HKFeedbackGraphViewクラスにノード作成機能と互換ポート取得機能を追加しました。

--- Assets/HKFeedback/Editor/HKFeedbackGraphView.cs
+++ Assets/HKFeedback/Editor/HKFeedbackGraphView.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.Experimental.GraphView;
+using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.UIElements;
 
@@ -15,14 +18,47 @@ namespace HKFeedback.Editor
             this.AddManipulator(new SelectionDragger());
             this.AddManipulator(new RectangleSelector());
             this.AddManipulator(new ContentDragger());
+            this.AddManipulator(new ContextualMenuManipulator(BuildContextualMenu));
 
             var background = new GridBackground();
             Insert(0, background);
+            background.StretchToParentSize();
+            background.SendToBack();
 
             var path = "Assets/HKFeedback/Editor/Settings/HKFeedbackGraphView.uss";
             var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(path);
             Assert.IsNotNull(styleSheet, $"{path} not found");
             styleSheets.Add(styleSheet);
+        }
+
+        public override void BuildContextualMenu(ContextualMenuPopulateEvent evt)
+        {
+            var position = viewTransform.matrix.inverse.MultiplyPoint(evt.localMousePosition);
+            evt.menu.AppendAction("Create Node", action => CreateNode("Node", position));
+            base.BuildContextualMenu(evt);
+        }
+
+        public override List<Port> GetCompatiblePorts(Port startPort, NodeAdapter nodeAdapter)
+        {
+            var compatiblePorts = new List<Port>(ports.Count());
+            foreach (var port in ports)
+            {
+                if (startPort.node == port.node || startPort.direction == port.direction || startPort.portType != port.portType)
+                {
+                    continue;
+                }
+                compatiblePorts.Add(port);
+            }
+            return compatiblePorts;
+        }
+
+        private Node CreateNode(string title, Vector2 position)
+        {
+            var node = new CustomNode(title);
+            node.styleSheets.Add(EditorGUIUtility.Load("StyleSheets/GraphView/Node.uss") as StyleSheet);
+            node.SetPosition(new Rect(position, new Vector2(150, 100)));
+            AddElement(node);
+            return node;
         }
     }
 }
