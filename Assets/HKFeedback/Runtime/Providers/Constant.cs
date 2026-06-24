bf4a30c2916636fb6bc33f485f732973695d6d97
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-06-25 01:16:07 +0900

    Constantクラスとそのメタファイルを新規作成しました。

---
+++ Assets/HKFeedback/Runtime/Providers/Constant.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+namespace HKFeedback.Providers
+{
+    [Serializable]
+    public class Constant<TContext> : IProvider<TContext>
+    {
+        [SerializeField]
+        private TContext context;
+
+        public Constant(TContext context)
+        {
+            this.context = context;
+        }
+
+        public TContext Provide() => context;
+    }
+}
